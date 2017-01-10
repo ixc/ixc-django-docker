0ad4c24a6fa8ccd496f7321e8884e6c0e46d40e0
Tai Lee <real.human@mrmachine.net>
2017-01-11 10:59:49 +1100

    Disable auto-reload under Docker for Mac to avoid high CPU utilisation.
    See: https://docs.docker.com/docker-for-mac/osxfs/#/performance-issues-solutions-and-roadmap

--- ixc_django_docker/bin/runserver.sh
+++ ixc_django_docker/bin/runserver.sh
@@ -6,4 +6,12 @@ EOF
 
 set -e
 
-exec manage.py runserver "${@:-0.0.0.0:8000}"
+if [[ -n "${DOCKER_FOR_MAC+1}" ]]; then
+	cat <<-EOF
+	Disable auto-reload under Docker for Mac to avoid high CPU utilisation.
+	See: https://docs.docker.com/docker-for-mac/osxfs/#/performance-issues-solutions-and-roadmap
+	EOF
+	set -- --noreload "$@"
+fi
+
+exec manage.py runserver "$@" 0.0.0.0:8000
