86b2e00eea1792b2e526167cd9f8fddf497a7838
Tai Lee <real.human@mrmachine.net>
2017-03-11 13:11:19 +1100

    Add support for basic auth in nginx config.
    Define both `BASIC_AUTH_USERNAME` and `BASIC_AUTH_PASSWORD` environment
    variables to enable.

--- ixc_django_docker/bin/nginx.sh
+++ ixc_django_docker/bin/nginx.sh
@@ -6,4 +6,12 @@ EOF
 
 set -e
 
+# Generate htpasswd file if credentials are set.
+if [[ -n "$BASIC_AUTH_USERNAME" && -n "$BASIC_AUTH_PASSWORD" ]]; then
+	echo "$BASIC_AUTH_PASSWORD" | htpasswd -ci "$IXC_DJANGO_DOCKER_DIR/etc/nginx.htpasswd" "$BASIC_AUTH_USERNAME"
+fi
+
+# Render nginx config template.
+dockerize -template "$IXC_DJANGO_DOCKER_DIR/etc/nginx.tmpl.conf:$IXC_DJANGO_DOCKER_DIR/etc/nginx.conf"
+
 exec nginx -c "$IXC_DJANGO_DOCKER_DIR/etc/nginx.conf" "$@"
