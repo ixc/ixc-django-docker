7e7d1fac98b18dfebe6a5da8f8a6990bd35fa2d8
Tai Lee <real.human@mrmachine.net>
2017-04-20 17:50:56 +1000

    Use async worker class (gevent), because we often block on IO.
    This should prevent the `CRITICAL WORKER TIMEOUT` issue that we were
    previously attempting to work-around by increasing the worker timeout to
    60s, and prevent requests from backing up and timing out when they are
    blocked waiting for IO (e.g. we made an external API/web request).
    
    You will need to `pip install gevent`.

--- ixc_django_docker/bin/gunicorn.sh
+++ ixc_django_docker/bin/gunicorn.sh
@@ -9,4 +9,4 @@ set -e
 # See: http://docs.gunicorn.org/en/stable/design.html#how-many-workers
 let "GUNICORN_WORKERS = ${GUNICORN_WORKERS:-${CPU_CORES:-1} * 2 + 1}"
 
-exec gunicorn --bind 0.0.0.0:8080 --timeout "${GUNICORN_TIMEOUT:-60}" --workers "$GUNICORN_WORKERS" "${@:-ixc_django_docker.wsgi:application}"
+exec gunicorn --bind 0.0.0.0:8080 --worker-class gevent --workers "$GUNICORN_WORKERS" "${@:-ixc_django_docker.wsgi:application}"
