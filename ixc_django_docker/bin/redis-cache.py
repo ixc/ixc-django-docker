6db065f2adee940c2bff2d9b85bd2a9fd09c44df
James Murty <james@murty.co>
2017-08-07 10:42:20 +1000

    Add logging and --quiet & --verbose arguments
    * --quiet option disables output to STDOUT
    * --verbose option increases verbosity of logging
      output
    * added logging messages for all actions.
    
    Log level is WARN by default.

--- ixc_django_docker/bin/redis-cache.py
+++ ixc_django_docker/bin/redis-cache.py
@@ -20,10 +20,12 @@ DEFAULT_EXPIRE_TIMEOUT_SECS = None
 ACTION_CHOICES = ['set', 'match', 'get', 'delete']
 
 
-def fault(parser, message=None):
+def fault(parser, logger, message=None):
     if message:
+        logger.error(message)
         sys.stderr.write(message + '\n')
     else:
+        logger.error("Invalid use of program, printing usage instructions")
         parser.print_help()
     sys.exit(1)
 
@@ -51,7 +53,8 @@ def main():
             "       read from STDIN if the [-x] option is set.\n"
             "\n"
             "'get': echo value in Redis for <key> to STDOUT.\n"
-            "       Returns ERRORLEVEL 100 if there is no such key in Redis.\n"
+            "       Returns ERRORLEVEL 100 if there is no such key in Redis,\n"
+            "       so this action also works as an existance check.\n"
             "\n"
             "'delete': delete <key> value from Redis. The <key> value does\n"
             "       not need to exist.\n"
@@ -99,7 +102,38 @@ def main():
         default=False,
         help='Read last argument from STDIN',
     )
+    parser.add_argument(
+        '-q',
+        '--quiet',
+        action='store_true',
+        help='Silence standard output',
+    )
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='count',
+        default=0,
+        dest='verbosity',
+        help='Increase verbosity for each occurrence',
+    )
     args = parser.parse_args()
+
+    # Configure log level with verbosity argument.
+    levels = (
+        # logging.CRITICAL,
+        # logging.ERROR,
+        logging.WARNING,
+        logging.INFO,
+        logging.DEBUG,
+    )
+    try:
+        logger.setLevel(levels[args.verbosity])
+    except IndexError:
+        logger.setLevel(logging.DEBUG)
+    # Silence standard output.
+    if args.quiet:
+        logger.debug("Suppressing STDOUT because the [--quiet] option is set")
+        sys.stdout = open(os.devnull, 'w')
 
     # Read input data from <value> argument or STDIN as requested, and
     # sanity-check argument values and combinations
@@ -109,6 +143,7 @@ def main():
         elif not args.value:
             fault(
                 parser,
+                logger,
                 "You must provide <value> as a third argument or read from"
                 " STDIN by setting the [-x] option"
             )
@@ -118,6 +153,7 @@ def main():
         if args.value or args.read_from_stdin:
             fault(
                 parser,
+                logger,
                 "You cannot provide <value> as an argument or read from STDIN"
                 " by setting the [-x] option for get actions"
             )
@@ -128,24 +164,43 @@ def main():
 
     if args.action == 'set':
         redis_set(conn, args.key, input_data, args.expire_secs)
+        logger.debug(
+            "Set Redis key '%s' to value: %s" % (args.key, input_data))
     elif args.action == 'get':
         cached_data = redis_get(conn, args.key)
+        logger.debug(
+            "Get for Redis key '%s' returned value: %s"
+            % (args.key, cached_data))
         if cached_data is None:
             sys.exit(100)
         else:
             sys.stdout.write(cached_data + "\n")
     elif args.action == 'delete':
         conn.delete(args.key)
+        logger.debug("Deleted Redis key '%s'" % args.key)
     elif args.action == 'match':
         # Read cached data
         cached_data = redis_get(conn, args.key)
         # Does cached data match input data?
+        logger.debug(
+            "Checking for match for Redis key '%s' and input value: %s"
+            % (args.key, input_data))
         if cached_data == input_data:
+            logger.info("Match result YES for Redis key '%s'" % args.key)
             return
         else:
+            logger.info("Match result NO for Redis key '%s'" % args.key)
             sys.exit(100)
     else:
         raise Exception("Unimplemented action '%s'" % args.action)
 
 if __name__ == '__main__':
-    main()
+    # Store original standard output destination
+    orig_stdout = sys.stdout
+    try:
+        main()
+    except:
+        raise
+    finally:
+        # Restore original standard output.
+        sys.stdout = orig_stdout
