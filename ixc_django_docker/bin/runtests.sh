3d822ce0674db1809077c6f17e98a55e119f89fd
James Murty <james@murty.co>
2017-10-24 15:11:14 +1100

    Allow tests to be run without compress & coverage steps
    - if WITHOUT_COMPRESSOR envvar is set, do not run
      the compress steps
    - if WITHOUT_COVERAGE envvar is set, do not run
      the coverage steps

--- ixc_django_docker/bin/runtests.sh
+++ ixc_django_docker/bin/runtests.sh
@@ -14,13 +14,18 @@ source setup-tests.sh
 # Only collect and compress static files when QUICK is not set or when it has
 # never been done before.
 [[ -z "$QUICK" || ! -d "$PROJECT_DIR/static_root" ]] && manage.py collectstatic --noinput --verbosity=0
-[[ -z "$QUICK" || ! -f "$PROJECT_DIR/static_root/CACHE/manifest.json" ]] && manage.py compress --force --verbosity=0
+
+if [[ -z "$WITHOUT_COMPRESSOR" ]]; then
+  [[ -z "$QUICK" || ! -f "$PROJECT_DIR/static_root/CACHE/manifest.json" ]] && manage.py compress --force --verbosity=0
+fi
 
 # Run tests, collecting coverage data and generate a coverage report.
-coverage run "$IXC_DJANGO_DOCKER_DIR/bin/manage.py" test --noinput --verbosity=2 "${@:-.}"
-coverage report
+if [[ -z "$WITHOUT_COMPRESSOR" ]]; then
+    coverage run "$IXC_DJANGO_DOCKER_DIR/bin/manage.py" test --noinput --verbosity=2 "${@:-.}"
+    coverage report
 
-# Submit coverage data to coveralls.
-if [[ -n "$TRAVIS" ]]; then
-	coveralls || true  # Don't exit if we can't submit data
+    # Submit coverage data to coveralls.
+    if [[ -n "$TRAVIS" ]]; then
+        coveralls || true  # Don't exit if we can't submit data
+    fi
 fi
