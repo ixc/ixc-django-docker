fb12b7d506e52640d0302b92ebc8bf487c209247
Tai Lee <real.human@mrmachine.net>
2017-11-30 14:00:51 +1100

    Export `PGDATABASE` from `setup-tests.sh` so it's persistent in subshells.

--- ixc_django_docker/bin/setup-tests.sh
+++ ixc_django_docker/bin/setup-tests.sh
@@ -11,6 +11,7 @@ set -e
 
 export OVERRIDE_SETTINGS="test.py"
 export REUSE_DB=1
+export PGDATABASE="test_$PGDATABASE"
 export PS1="($PROJECT_NAME:test)\w\$ "
 export SETUP_TESTS=1
 export SRC_PGDATABASE="${SRC_PGDATABASE:-$PROJECT_DIR/test_initial_data.sql}"
@@ -20,7 +21,8 @@ DJANGO_VERSION_LESS_THAN_1_7=$(python -c 'import django; print(django.VERSION < 
 # Only drop existing database when QUICK is not set.
 [[ -z "$QUICK" ]] && export SETUP_POSTGRES_FORCE=1
 
-PGDATABASE="test_$PGDATABASE" setup-postgres.sh
+setup-postgres.sh
+
 if [[ DJANGO_VERSION_LESS_THAN_1_7 == 'True' ]]; then
 	echo 'Always sync database, because Django version is less than 1.7.'
 	manage.py syncdb --noinput
