0ce70f2fee796ad31456532e7164032cd485754a
Tai Lee <real.human@mrmachine.net>
2017-12-07 09:28:39 +1100

    Add staging settings which are just like production, but don't actually send email.

---
+++ ixc_django_docker/settings/staging.py
@@ -0,0 +1,17 @@
+# Keep more log backups. Enable LogEntries log handler.
+LOGGING['handlers']['logfile']['backupCount'] = 100
+LOGGING['loggers']['']['handlers'].append('logentries')
+
+# Enable the per-site cache.
+MIDDLEWARE_CLASSES = \
+    ('django.middleware.cache.UpdateCacheMiddleware', ) + \
+    MIDDLEWARE_CLASSES + \
+    ('django.middleware.cache.FetchFromCacheMiddleware', )
+
+# Enable the cached template loader.
+TEMPLATES[0]['OPTIONS']['loaders'] = [
+    (
+        'django.template.loaders.cached.Loader',
+        TEMPLATES[0]['OPTIONS']['loaders'],
+    ),
+]
