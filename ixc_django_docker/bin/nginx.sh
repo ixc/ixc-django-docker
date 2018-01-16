6dff259d54ed314cf5be0802aa79fa63f31d81d3
Tai Lee <real.human@mrmachine.net>
2018-01-16 13:55:42 +1100

    Work-around permission denied error (error log) when running nginx as unprivileged user.
    `/var/log/nginx/error.log` is often specified when nginx is compiled,
    and nginx will attempt to open this log file before its configuration
    file is read (presumably so it can log configuration errors).
    
    When running as an unprivileged user, a permission denied error will
    prevent startup.
    
    The solution is to specify a global configuration directive via the
    `-g` command line option, and remove `error_log` from the config file.
    
    See: https://stackoverflow.com/a/24423319

--- ixc_django_docker/bin/nginx.sh
+++ ixc_django_docker/bin/nginx.sh
@@ -15,4 +15,6 @@ fi
 # Render nginx config template.
 dockerize -template "$IXC_DJANGO_DOCKER_DIR/etc/nginx.tmpl.conf:$IXC_DJANGO_DOCKER_DIR/etc/nginx.conf"
 
-exec nginx -c "$IXC_DJANGO_DOCKER_DIR/etc/nginx.conf" "$@"
+# `error_log` needs to be set via command line to avoid a permissions error when
+# run as an unprivileged user. See: https://stackoverflow.com/a/24423319
+exec nginx -c "$IXC_DJANGO_DOCKER_DIR/etc/nginx.conf" -g "error_log stderr;" "$@"
