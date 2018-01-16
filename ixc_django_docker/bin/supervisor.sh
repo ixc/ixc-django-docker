0f839445ecf4512f90fbf9b3dc163797ed32539d
Tai Lee <real.human@mrmachine.net>
2018-01-16 15:49:46 +1100

    Run supervisor on a random-ish port, derived from $PROJECT_DIR.
    This allows us to run `supervisor.sh` for multiple projects on a server
    without isolation.
    
    If there is a conflict, you can explicitly set `SUPERVISORD_PORT` in your
    dotenv file.

--- ixc_django_docker/bin/supervisor.sh
+++ ixc_django_docker/bin/supervisor.sh
@@ -8,6 +8,9 @@ EOF
 
 set -e
 
+# Get a random-ish port in the 59000-59999 range. See: https://github.com/rfk/django-supervisor/blob/be2013c4826ae49730664b359ee285fa03b16c09/djsupervisor/config.py#L107-L111
+export SUPERVISORD_PORT="${SUPERVISORD_PORT:-59$(python -c "import hashlib; print('%03d' % (int(hashlib.md5('$PROJECT_DIR').hexdigest()[:3], 16) % 1000));")}"
+
 # Render config templates.
 dockerize -template "${SUPERVISORD_TMPL_CONF:-$IXC_DJANGO_DOCKER_DIR/etc/supervisord.tmpl.conf}:$PROJECT_DIR/var/etc/supervisord.conf"
 dockerize -template "${SUPERVISORD_INCLUDE_TMPL_CONF:-$IXC_DJANGO_DOCKER_DIR/etc/supervisord.nginx-proxy.tmpl.conf}:$PROJECT_DIR/var/etc/supervisord.include.conf"
