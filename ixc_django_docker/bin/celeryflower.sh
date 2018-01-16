e03750f0e1d7722eff7c6e0a35af237f2eaae69c
Tai Lee <real.human@mrmachine.net>
2018-01-16 13:39:43 +1100

    Run Celery Flower on `NGINX_PROXY_PORT` (default: 8080).
    When running multiple processes without isolation, we need to run them
    on different ports.

--- ixc_django_docker/bin/celeryflower.sh
+++ ixc_django_docker/bin/celeryflower.sh
@@ -6,4 +6,4 @@ EOF
 
 set -e
 
-exec newrelic.sh celery --app="${CELERY_APP:-ixc_django_docker.celery}" flower --port=8080 "$@"
+exec newrelic.sh celery --app="${CELERY_APP:-ixc_django_docker.celery}" flower --port=${NGINX_PROXY_PORT:-8080} "$@"
