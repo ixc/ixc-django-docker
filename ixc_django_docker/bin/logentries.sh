1cc5584f66cfab58e32842e7043855201991a0c1
Tai Lee <real.human@mrmachine.net>
2018-02-01 15:07:37 +1100

    Ensure that `HOSTNAME` is set, and prefix with the project (dir) and dotenv file names.
    Because hostnames like `django-1` are not very helpful when we have
    multiple projects.

--- ixc_django_docker/bin/logentries.sh
+++ ixc_django_docker/bin/logentries.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Ensure HOSTNAME is set, and prefix with project and dotenv file name.
+export HOSTNAME="$PROJECT_NAME-$DOTENV.${HOSTNAME:-$(hostname)}"
+
 # Render logentries config template.
 mkdir -p "$PROJECT_DIR/var/etc"
 dockerize -template "${LOGENTRIES_TMPL_CONF:-$IXC_DJANGO_DOCKER_DIR/etc/logentries.tmpl.conf}:$PROJECT_DIR/var/etc/logentries.conf"
