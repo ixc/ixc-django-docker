aa502ded32f6fe735c7d5ae55d26cd7bd869c739
Tai Lee <real.human@mrmachine.net>
2018-03-07 00:10:30 +1100

    Don't set default cache backend to Redis (without also setting a location) in test settings.
    The default cache backend is set to Redis in `redis_cache.py` which should
    be included via `BASE_SETTINGS` in `.env.base` or, if doing so prevents
    `Dockerfile` from building, via `include()` in `develop.py`, `production.py`,
    `staging.py`, and `test.py`.

--- ixc_django_docker/settings/test.py
+++ ixc_django_docker/settings/test.py
@@ -10,7 +10,6 @@ if not os.environ.get('SETUP_TESTS'):
 # DJANGO ######################################################################
 
 ALLOWED_HOSTS = ('*', )
-CACHES['default'].update({'BACKEND': 'redis_lock.django_cache.RedisCache'})
 
 CSRF_COOKIE_SECURE = False  # Don't require HTTPS for CSRF cookie
 SESSION_COOKIE_SECURE = False  # Don't require HTTPS for session cookie
