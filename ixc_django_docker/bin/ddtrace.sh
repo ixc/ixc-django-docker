a163ea4c3da316ef0b40ee116345c7ab09f24955
Tai Lee <real.human@mrmachine.net>
2018-06-06 23:18:45 +1000

    Use `$PROJECT_NAME-$DOTENV` as `DATADOG_ENV` instead of just `$DOTENV`, which is ambiguous.

--- ixc_django_docker/bin/ddtrace.sh
+++ ixc_django_docker/bin/ddtrace.sh
@@ -6,7 +6,7 @@ EOF
 
 set -e
 
-export DATADOG_ENV="${DATADOG_ENV:-$DOTENV}"
+export DATADOG_ENV="${DATADOG_ENV:-$PROJECT_NAME-${DATADOG_ENV:-$DOTENV}}"
 export DATADOG_SERVICE_NAME="${DATADOG_SERVICE_NAME:-$PROJECT_NAME}"
 
 echo 'Run command via Datadog Trace Client.'
