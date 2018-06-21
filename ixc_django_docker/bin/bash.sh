639e605b6f1716d7fa73ceb1cf8cc765b268f7c8
Tai Lee <real.human@mrmachine.net>
2018-06-21 11:16:47 +1000

    Sleep forever when `bash.sh` is run without a tty, so we can exec into a container to troubleshoot.

--- ixc_django_docker/bin/bash.sh
+++ ixc_django_docker/bin/bash.sh
@@ -8,7 +8,8 @@ EOF
 
 set -e
 
-cat <<EOF
+if [[ -t 1 ]]; then
+	cat <<EOF
 
 You are running a pre-onfigured interactive Bash shell. Here is a list of
 frequently used scripts you might want to run:
@@ -52,8 +53,22 @@ See more detailed documentation about commands at:
 
 EOF
 
-export PS1="($PROJECT_NAME) \u@\h:\w\\n\$ "
+	export PS1="($PROJECT_NAME) \u@\h:\w\\n\$ "
 
-# Run bash by default without any user customisations from rc or profile files
-# to reduce the chance of user customisations clashing with our paths etc.
-exec bash --norc --noprofile
+	# Run bash by default without any user customisations from rc or profile files
+	# to reduce the chance of user customisations clashing with our paths etc.
+	exec bash --norc --noprofile
+else
+	cat <<EOF
+
+Sleeping forever, so you can exec into this container to troubleshoot.
+
+For example, you might need to rollback database migrations before rolling back
+a deployment.
+
+Or you might want to install and temporarily run a program that needs access to
+the stack, and exposes a service on a dynamic port.
+
+EOF
+	sleep infinity
+fi
