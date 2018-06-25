aa064abc0550c7830c494a93ff9dcf79ebfc586d
Tai Lee <real.human@mrmachine.net>
2018-06-25 14:43:23 +1000

    Failfast and exit from `go.sh` before installing venv if `.env.local` does not exist.

--- project_template/go.sh
+++ project_template/go.sh
@@ -15,6 +15,14 @@ export PROJECT_DIR=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd -P)
 # Set location of virtualenv.
 export PROJECT_VENV_DIR="${VIRTUAL_ENV:-$PROJECT_DIR/var/go.sh-venv}"
 
+# Source local environment variables.
+if [[ -f "$PROJECT_DIR/.env.local" ]]; then
+	source "$PROJECT_DIR/.env.local"
+else
+	>&2 echo 'ERROR: You must create a `.env.local` file. See: `.env.local.sample`.'
+	exit 1
+fi
+
 # Create virtualenv.
 if [[ ! -d "$PROJECT_VENV_DIR" ]]; then
 	virtualenv "$PROJECT_VENV_DIR"
@@ -24,14 +32,6 @@ fi
 if [[ ! -s requirements.txt.md5 ]] || ! md5sum --status -c requirements.txt.md5 > /dev/null 2>&1; then
 	"$PROJECT_VENV_DIR/bin/python" -m pip install --no-cache-dir --no-deps -e . -r <(grep -v setuptools requirements.txt)  # Unpin setuptools dependencies. See: https://github.com/pypa/pip/issues/4264
 	md5sum requirements.txt > requirements.txt.md5
-fi
-
-# Source local environment variables.
-if [[ -f "$PROJECT_DIR/.env.local" ]]; then
-	source "$PROJECT_DIR/.env.local"
-else
-	>&2 echo 'ERROR: You must create a `.env.local` file. See: `.env.local.sample`.'
-	exit 1
 fi
 
 # Execute entrypoint and command.
