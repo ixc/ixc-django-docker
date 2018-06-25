89ddcad2142d0d6c69699395516321ab8c08c9ea
Tai Lee <real.human@mrmachine.net>
2018-06-25 14:10:51 +1000

    Treat `supervisord` (Python 2 only) as a system dependency, not a project dependency.

--- ixc_django_docker/bin/entrypoint.sh
+++ ixc_django_docker/bin/entrypoint.sh
@@ -87,7 +87,7 @@ else
 	done
 
 	# Fail loudly when required programs are missing.
-	for cmd in md5sum nginx npm psql python pv redis-server yarn dockerize; do  # TODO: elasticsearch git-secret transcrypt
+	for cmd in md5sum nginx npm psql python pv redis-server supervisord supervisorctl yarn dockerize; do  # TODO: elasticsearch git-secret transcrypt
 		hash $cmd 2>/dev/null || {
 			>&2 echo "ERROR: Missing program: $cmd"
 			>&2 echo 'See: https://github.com/ixc/ixc-django-docker/blob/master/README.rst#system-requirements-when-running-without-docker'
