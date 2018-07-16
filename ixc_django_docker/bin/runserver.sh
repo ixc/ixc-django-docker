e2e0bd53624f043e977f46f68e97eca5a7167eb6
Tai Lee <real.human@mrmachine.net>
2018-07-17 00:37:51 +1000

    Specify app module.

--- ixc_django_docker/bin/runserver.sh
+++ ixc_django_docker/bin/runserver.sh
@@ -5,4 +5,4 @@ set -e
 export NGINX_PROXY_PORT="${NGINX_PROXY_PORT:-8000}"
 export GUNICORN_WORKERS=1
 
-exec gunicorn.sh --access-logformat '%(t)s "%(r)s" %(s)s %(b)s' --reload "$@"
+exec gunicorn.sh --access-logformat '%(t)s "%(r)s" %(s)s %(b)s' --reload "${@:-ixc_django_docker.wsgi:application}"
