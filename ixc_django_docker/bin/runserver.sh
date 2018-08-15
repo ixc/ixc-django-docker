0d31edeea8932ab1db9893e6805f4fdb90791448
Tai Lee <real.human@mrmachine.net>
2018-08-15 10:57:13 +1000

    1 gunicorn worker by default for `runserver.sh`.
    Can be overridden with `GUNICORN_WORKERS` e.g. when using sync worker
    class and needing to test concurrent/AJAX requests.

--- ixc_django_docker/bin/runserver.sh
+++ ixc_django_docker/bin/runserver.sh
@@ -3,6 +3,6 @@
 set -e
 
 export NGINX_PROXY_PORT="${NGINX_PROXY_PORT:-8000}"
-export GUNICORN_WORKERS=1
+export GUNICORN_WORKERS="${GUNICORN_WORKERS:-1}"
 
 exec gunicorn.sh --access-logformat '%(t)s "%(r)s" %(s)s %(b)s' --reload "${@:-ixc_django_docker.wsgi:application}"
