bdc3011a689830a50990627720bae894d4c0c028
Tai Lee <real.human@mrmachine.net>
2018-10-25 14:00:09 +1100

    Update email subject to include the dotenv name and site domain, to make it clear this is a hijacked email.

--- ixc_django_docker/settings/email_bandit.py
+++ ixc_django_docker/settings/email_bandit.py
@@ -1,6 +1,11 @@
 HIJACKED_EMAIL_BACKEND = EMAIL_BACKEND
 
 EMAIL_BACKEND = 'ixc_django_docker.bandit.HijackedEmailBackend'
+
+# Make it clear that the email has been hijacked and the environment frmo which
+# it has originated.
+EMAIL_SUBJECT_PREFIX = '[%s:%s] ' % (DOTENV.upper(), SITE_DOMAIN)
+
 INSTALLED_APPS += ('bandit', )
 
 # Make sure we are hijacking a backend that can actually send emails (e.g. to
