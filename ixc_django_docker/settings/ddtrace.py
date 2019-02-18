7e49195ae139fb88f1791eac4804907708b721d9
Tai Lee <real.human@mrmachine.net>
2019-02-18 16:54:34 +1100

    Update link to docs about dd-trace environment config.

--- ixc_django_docker/settings/ddtrace.py
+++ ixc_django_docker/settings/ddtrace.py
@@ -1,7 +1,7 @@
 # See: http://pypi.datadoghq.com/trace/docs/#module-ddtrace.contrib.django
 
 # Configure with environment variables. See 'ddtrace.sh' and
-# https://github.com/DataDog/dd-trace-py/blob/master/docs/index.rst#get-started
+# http://pypi.datadoghq.com/trace/docs/advanced_usage.html#ddtracerun
 DATADOG_TRACE = {}
 
 INSTALLED_APPS += ('ddtrace.contrib.django', )
