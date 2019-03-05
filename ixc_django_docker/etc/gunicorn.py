5f3d48a0c7e6e49c9cada6039b56324e5bfe4041
Tai Lee <real.human@mrmachine.net>
2019-03-06 09:49:17 +1100

    Preload workers by default, to speed up first request and reduce memory consumption.

--- ixc_django_docker/etc/gunicorn.py
+++ ixc_django_docker/etc/gunicorn.py
@@ -1,10 +1,16 @@
 import os
 import multiprocessing
 
+
+def truthy(val):
+    return unicode(val) in ('1', 'on', 't', 'true', 'y', 'yes')
+
+
 accesslog = os.environ.get('GUNICORN_ACCESS_LOG', '-') or None
 bind = '0.0.0.0:%s' % os.environ.get('NGINX_PROXY_PORT', 8080)
 max_requests = os.environ.get('GUNICORN_MAX_REQUESTS', 10000)
 max_requests_jitter = os.environ.get('GUNICORN_MAX_REQUESTS_JITTER', 1000)
+preload = truthy(os.environ.get('GUNICORN_PRELOAD', 'true'))
 threads = os.environ.get('GUNICORN_THREADS', 1)
 timeout = os.environ.get('GUNICORN_TIMEOUT', 50)  # HAproxy default
 worker_class = os.environ.get('GUNICORN_WORKER_CLASS', 'sync')
