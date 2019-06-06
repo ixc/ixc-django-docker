6be8fee2f9f2cabb2a12cead1501b39f92137125
Tai Lee <real.human@mrmachine.net>
2019-06-07 01:26:29 +1000

    Simplify `entrypoint.sh`.
    * Always Fail if required environment variables or programs are missing.
    * Require `PGDATABASE` environment variable, and remove default derived from `${PROJECT_NAME}_${DOTENV}`.
    * Do not source hierarchical `.env.*` files. Only source `$PROJECT_DIR/.envrc`.
    * Remove transcrypt support. The encrypted `.envrc` file in the project repo is only used for development, and the repo must be manually initialised to support transcrypt before executing `entrypoint.sh`. Production and staging secrets have been moved to the stacks repo.
    * Make no assumptions about the configuration or location of a virtualenv. The user must activate any virtualenv manually, if required, before executing `entrypoint.sh`.
    * Remove `CPU_CORES` export. We prefer to specify an explicit number of worker processes, now.
    
    When run with Docker:
    * Add missing Python 3.7 site-packages directory when run with Docker.
    * Configure the alternate installation user scheme for Python when run with Docker.
    * Overwrite instead of appending `host.docker.internal` record when run with Docker.
    * Remove Docker for Mac performance warning.

--- ixc_django_docker/bin/entrypoint.sh
+++ ixc_django_docker/bin/entrypoint.sh
@@ -9,8 +9,25 @@ if [[ -d .git ]]; then
 	echo "Git Commit: $(git rev-parse HEAD)"
 fi
 
+# Fail loudly when required environment variables are missing.
+for var in PROJECT_DIR PGDATABASE; do
+	eval [[ -z \${$var+1} ]] && {
+		>&2 echo "ERROR: Missing environment variable: $var"
+		exit 1
+	}
+done
+
+# Fail loudly when required programs are missing.
+for cmd in dockerize md5sum nginx npm psql python pv redis-server supervisord supervisorctl transcrypt; do  # TODO: elasticsearch yarn
+	hash $cmd 2>/dev/null || {
+		>&2 echo "ERROR: Missing program: $cmd"
+		>&2 echo 'See: https://github.com/ixc/ixc-django-docker/blob/master/README.rst#system-requirements-when-running-without-docker'
+		exit 1
+	}
+done
+
 if [[ -f /.dockerenv ]]; then
-	# When run via Docker, the only system site packages are the ones that we
+	# When run with Docker, the only system site packages are the ones that we
 	# have installed, so we do not need a virtualenv for isolation. Using an
 	# isolated virtualenv would mean we have to reinstall everything during
 	# development, even when no versions have changed. Using a virtualenv
@@ -44,57 +61,11 @@ if [[ -f /.dockerenv ]]; then
 	mkdir -p "$PYTHONUSERBASE/lib/python2.7/site-packages"
 	mkdir -p "$PYTHONUSERBASE/lib/python3.5/site-packages"
 	mkdir -p "$PYTHONUSERBASE/lib/python3.6/site-packages"
+	mkdir -p "$PYTHONUSERBASE/lib/python3.7/site-packages"
 
 	# Ensure host is accessible at `host.docker.internal`.
+	grep -v host.docker.internal /etc/hosts > /etc/hosts
 	ip -4 route list match 0/0 | awk '{print $3" host.docker.internal"}' >> /etc/hosts
-
-	# On Docker for Mac, osxfs has performance issues when watching file system
-	# events. Detect Docker for Mac and export an environment variable that we
-	# can check to disable file system watches.
-	if mount | grep -q osxfs; then
-		cat <<-EOF
-		#
-		# IMPORTANT
-		#
-		# You are running Docker for Mac with shared volumes, which has
-		# performance issues causing high CPU utilisation. See:
-		#
-		#     https://docs.docker.com/docker-for-mac/osxfs/#/performance-issues-solutions-and-roadmap
-		#
-		# You should avoid anything that watches the file system. For example,
-		# the Django dev server with auto-reloading enabled.
-		#
-		# You can check the 'DOCKER_FOR_MAC=1' environment variable to
-		# conditionally disable any such features. The 'runserver.sh' script
-		# already does this.
-		#
-		EOF
-		export DOCKER_FOR_MAC=1
-	fi
-else
-	# When run via 'go.sh', we need a virtualenv for isolation from system site
-	# packages, and we also verify that required environment variables and
-	# programs are available.
-
-	# Fail loudly when required environment variables are missing.
-	for var in PROJECT_DIR PROJECT_VENV_DIR; do
-		eval [[ -z \${$var+1} ]] && {
-			>&2 echo "ERROR: Missing environment variable: $var"
-			exit 1
-		}
-	done
-
-	# Fail loudly when required programs are missing.
-	for cmd in dockerize md5sum nginx npm psql python pv redis-server supervisord supervisorctl transcrypt yarn; do  # TODO: elasticsearch
-		hash $cmd 2>/dev/null || {
-			>&2 echo "ERROR: Missing program: $cmd"
-			>&2 echo 'See: https://github.com/ixc/ixc-django-docker/blob/master/README.rst#system-requirements-when-running-without-docker'
-			exit 1
-		}
-	done
-
-	# Add virtualenv bin directory to PATH.
-	export PATH="$PROJECT_VENV_DIR/bin:$PATH"
 fi
 
 # Get absolute directory for the `ixc_django_docker` package. We can't use
@@ -105,82 +76,32 @@ export IXC_DJANGO_DOCKER_DIR="$("${PYTHON_VERSION:-python}" -c 'import ixc_djang
 # Add project, `node_modules`, and `ixc-django-docker` bin directories to PATH.
 export PATH="$PROJECT_DIR/bin:$PROJECT_DIR/node_modules/.bin:$IXC_DJANGO_DOCKER_DIR/bin:$PATH"
 
-# Source local dotenv file, which is not encrypted or version controlled, and
-# may contain the password needed to decrypt secret files.
-if [[ -f "$PROJECT_DIR/.env.local" ]]; then
-	set -o allexport
-	source "$PROJECT_DIR/.env.local"
-	set +o allexport
-fi
-
-# Decrypt files with transcrypt.
-if [[ -n "$TRANSCRYPT_PASSWORD" ]]; then
-	git status &> /dev/null  # See: https://github.com/elasticdog/transcrypt/issues/37
-	# Use `--force` to overwrite "missing" secrets that are listed in
-	# `.dockerignore` to avoid accidentally copying decrypted secrets into an
-	# image.
-	transcrypt --force -c "${TRANSCRYPT_CIPHER:-aes-256-cbc}" -p "$TRANSCRYPT_PASSWORD" -y || true  # Don't exit if we can't decrypt secrets
-fi
-
-# Source global, environment and local dotenv files, if decrypted.
-for dotenv in base "$DOTENV" "$DOTENV.secret" local; do
-	DOTENV_FILE="$PROJECT_DIR/.env.$dotenv"
-	if [[ -f "$DOTENV_FILE" ]]; then
-		echo "Sourcing DOTENV file: $DOTENV_FILE"
-		set -o allexport
-		source "$DOTENV_FILE"
-		set +o allexport
-	fi
-done
-
-# Get number of CPU cores, so we know how many processes to run.
-export CPU_CORES=$(python.sh -c "import multiprocessing; print(multiprocessing.cpu_count());")
-
 # Configure Pip.
-export PIP_DISABLE_PIP_VERSION_CHECK=on
-export PIP_SRC="${PIP_SRC:-$PROJECT_DIR/src}"
-
-# Get project name from the project directory.
-export PROJECT_NAME=$(basename "$PROJECT_DIR")
+export PIP_DISABLE_PIP_VERSION_CHECK='on'
 
 # Configure Python.
-export PYTHONDONTWRITEBYTECODE=1
-export PYTHONHASHSEED=random
+export PYTHONDONTWRITEBYTECODE='1'
+export PYTHONHASHSEED='random'
 export PYTHONPATH="$PROJECT_DIR:$PYTHONPATH"
-export PYTHONWARNINGS=ignore
+export PYTHONWARNINGS='ignore'
 
-# Set PostgreSQL database and port.
-export PGDATABASE="${PGDATABASE:-${PROJECT_NAME}_${DOTENV}}"
-export PGPORT="${PGPORT:-5432}"
-
-# Set overridable default environment variables.
+# Configure default environment variables when run with and without Docker.
 if [[ -f /.dockerenv ]]; then
-	# Set Datadog trace agent host.
 	export DATADOG_TRACE_AGENT_HOSTNAME="${DATADOG_TRACE_AGENT_HOSTNAME:-datadog}"
-
-	# Set Elasticsearch host and port.
 	export ELASTICSEARCH_ADDRESS="${ELASTICSEARCH_ADDRESS:-elasticsearch:9200}"
-
-	# Set PostgreSQL host and user.
 	export PGHOST="${PGHOST:-postgres}"
 	export PGUSER="${PGUSER:-postgres}"
-
-	# Set Redis host and port.
 	export REDIS_ADDRESS="${REDIS_ADDRESS:-redis:6379}"
 else
-	# Set Datadog trace agent host.
 	export DATADOG_TRACE_AGENT_HOSTNAME="${DATADOG_TRACE_AGENT_HOSTNAME:-localhost}"
-
-	# Set Elasticsearch host and port.
 	export ELASTICSEARCH_ADDRESS="${ELASTICSEARCH_ADDRESS:-localhost:9200}"
-
-	# Set PostgreSQL host and user.
 	export PGHOST="${PGHOST:-localhost}"
 	export PGUSER="${PGUSER:-$(whoami)}"
-
-	# Set Redis host and port.
 	export REDIS_ADDRESS="${REDIS_ADDRESS:-localhost:6379}"
 fi
+
+# Configure project.
+source "$PROJECT_DIR/.envrc"
 
 # Execute command.
 exec "${@:-bash.sh}"
