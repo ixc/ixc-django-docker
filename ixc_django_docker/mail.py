3e7531b39235d570547ac9f805a67b285b1ddf69
Aram Dulyan <aram00@gmail.com>
2019-06-18 15:18:13 +1000

    Fix Django<1.9 compatibility in mail module

--- ixc_django_docker/mail.py
+++ ixc_django_docker/mail.py
@@ -1,4 +1,8 @@
 from django.conf import settings
-from django.utils.module_loading import import_string
+try:
+    # For Django<1.9 compatibility
+    from django.utils.module_loading import import_by_path as import_string
+except ImportError:
+    from django.utils.module_loading import import_string
 
 EmailBackend = import_string(settings.IXC_DJANGO_DOCKER_EMAIL_BACKEND)
