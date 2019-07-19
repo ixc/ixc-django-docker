552b37a563f9956f80e572b4aa26c356dc9450e3
James Murty <james@murty.co>
2019-07-19 11:26:26 +1000

    Further improve Bandit email hijacking
    - add support for BANDIT_WHITELIST envvar setting
      to allow whitelisting of email recipient(s)
      using a comma-delimited list
    - support multiple TO addresses for hijacked
      emails by processing as a comma-delimited list
    - ALWAYS print out the BANDIT_EMAIL and
      BANDIT_WHITELIST settings since these are *very*
      important on sites where we are trying to apply
      email hijacking by using the `eamil_bandit`
      composable setting.

--- ixc_django_docker/settings/email_bandit.py
+++ ixc_django_docker/settings/email_bandit.py
@@ -10,7 +10,33 @@ else:
     HIJACKED_EMAIL_BACKEND = EMAIL_BACKEND
     EMAIL_BACKEND = 'ixc_django_docker.bandit.HijackedEmailBackend'
 
-BANDIT_EMAIL = os.environ.get('BANDIT_EMAIL')
+# Hijack outgoing emails and send them to these email addresses instead.
+# We will generally only use one email address, but multiple are supported.
+# Specify envvar as a comma-delimited string, e.g.
+#     BANDIT_EMAIL='admins@interaction.net.au'
+if os.environ.get('BANDIT_EMAIL'):
+    BANDIT_EMAIL = [
+        email.strip()
+        for email in os.environ['BANDIT_EMAIL'].split(',')
+        if email.strip()
+    ]
+else:
+    BANDIT_EMAIL = None
+print("%s: BANDIT_EMAIL = %r" % (__name__, BANDIT_EMAIL))
+
+# Whitelist outgoing emails to these specific addresses or domains to let
+# them through, instead of redirecting them to the BANDIT_EMAIL address.
+# Specify envvar as a comma-delimited string, e.g.
+#     BANDIT_WHITELIST='interaction.net.au,user_abc@client.org.au'
+if os.environ.get('BANDIT_WHITELIST'):
+    BANDIT_WHITELIST = [
+        wl.strip()
+        for wl in os.environ['BANDIT_WHITELIST'].split(',')
+        if wl.strip()
+    ]
+    print("%s: BANDIT_WHITELIST = %r" % (__name__, BANDIT_WHITELIST))
+else:
+    print("%s: BANDIT_WHITELIST is not set" % __name__)
 
 # Make it clear that emails have been hijacked and from which site.
 # NOTE: This only applies to emails sent with admin-specific methods:
