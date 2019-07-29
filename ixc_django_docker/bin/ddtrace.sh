84129719008b01c9fae9cececf3d306e5839ed06
Tai Lee <real.human@mrmachine.net>
2019-07-29 10:34:31 +1000

    Switch to new `DD_TRACE_ANALYTICS_ENABLED` environment variable.

--- ixc_django_docker/bin/ddtrace.sh
+++ ixc_django_docker/bin/ddtrace.sh
@@ -3,7 +3,10 @@
 set -e
 
 export DATADOG_ENV="${DATADOG_ENV:-$PROJECT_NAME-${DATADOG_ENV:-$DOTENV}}"
-export DD_ANALYTICS_ENABLED='true'
+export DD_TRACE_ANALYTICS_ENABLED='true'
+
+# For ddtrace < 0.25.
+export DD_ANALYTICS_ENABLED="$DD_TRACE_ANALYTICS_ENABLED"
 
 echo 'Run command via Datadog Trace Client.'
 
