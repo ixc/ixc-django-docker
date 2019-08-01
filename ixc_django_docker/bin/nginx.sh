ac0a37efa056af6e81e0282581545463a98256ec
Tai Lee <real.human@mrmachine.net>
2019-08-02 02:45:08 +1000

    Add "context" support to `nginx.sh` and `supervisor.sh`. Store rendered configs in distinct subdirectories to avoid clobbering each other on a shared volume.

--- ixc_django_docker/bin/nginx.sh
+++ ixc_django_docker/bin/nginx.sh
@@ -2,11 +2,19 @@
 
 set -e
 
+# Create context specific config directory.
+if [[ -z "${CONTEXT+1}" ]]; then
+	DIR="$PROJECT_DIR/var/etc"
+else
+	DIR="$PROJECT_DIR/var/etc/$CONTEXT"
+fi
+mkdir -p "$DIR"
+
 # Generate htpasswd file if credentials are set.
 if [[ -n "$NGINX_BASIC_AUTH" ]]; then
 	# Split on `:`.
 	IFS=: read BASIC_AUTH_USERNAME BASIC_AUTH_PASSWORD <<< "$NGINX_BASIC_AUTH"
-	echo "$BASIC_AUTH_PASSWORD" | htpasswd -ci "$PROJECT_DIR/var/etc/nginx.htpasswd" "$BASIC_AUTH_USERNAME"
+	echo "$BASIC_AUTH_PASSWORD" | htpasswd -ci "$DIR/nginx.htpasswd" "$BASIC_AUTH_USERNAME"
 fi
 
 # Default environment variables for config template.
@@ -17,9 +25,8 @@ export NGINX_WORKER_PROCESSES="${NGINX_WORKER_PROCESSES:-1}"
 export SUPERVISOR_PROGRAM_NUMPROCS="${SUPERVISOR_PROGRAM_NUMPROCS:-1}"
 
 # Render nginx config template.
-mkdir -p "$PROJECT_DIR/var/etc"
-dockerize -template "${NGINX_TMPL_CONF:-$IXC_DJANGO_DOCKER_DIR/etc/nginx.tmpl.conf}:$PROJECT_DIR/var/etc/nginx.conf"
+dockerize -template "${NGINX_TMPL_CONF:-$IXC_DJANGO_DOCKER_DIR/etc/nginx.tmpl.conf}:$DIR/nginx.conf"
 
 # Set `error_log` via command line to avoid a permissions error when run as an
 # unprivileged user. See: https://stackoverflow.com/a/24423319
-exec nginx -c "$PROJECT_DIR/var/etc/nginx.conf" -g "error_log /dev/stderr;" "$@"
+exec nginx -c "$DIR/nginx.conf" -g "error_log /dev/stderr;" "$@"
--- ixc_django_docker/bin/supervisor.sh
+++ ixc_django_docker/bin/supervisor.sh
@@ -4,8 +4,17 @@
 
 set -e
 
-# Get a random-ish port in the 59000-59999 range. See: https://github.com/rfk/django-supervisor/blob/be2013c4826ae49730664b359ee285fa03b16c09/djsupervisor/config.py#L107-L111
-export SUPERVISOR_PORT="${SUPERVISOR_PORT:-59$(python.sh -c "import hashlib; print('%03d' % (int(hashlib.md5(u'$PROJECT_DIR'.encode('utf-8')).hexdigest()[:3], 16) % 1000));")}"
+# Create context specific config directory.
+if [[ -z "${CONTEXT+1}" ]]; then
+	DIR="$PROJECT_DIR/var/etc"
+else
+	DIR="$PROJECT_DIR/var/etc/$CONTEXT"
+fi
+mkdir -p "$DIR"
+
+# Get a random-ish port in the 59000-59999 range.
+# See: https://github.com/rfk/django-supervisor/blob/be2013c4826ae49730664b359ee285fa03b16c09/djsupervisor/config.py#L107-L111
+export SUPERVISOR_PORT="${SUPERVISOR_PORT:-59$(python.sh -c "import hashlib; print('%03d' % (int(hashlib.md5(u'$DIR'.encode('utf-8')).hexdigest()[:3], 16) % 1000));")}"
 
 # Default environment variables for config template.
 export NGINX_PROXY_PORT="${NGINX_PROXY_PORT:-8080}"
@@ -14,12 +23,11 @@ export SUPERVISOR_PROGRAM_ENVIRONMENT="${SUPERVISOR_PROGRAM_ENVIRONMENT:-}"
 export SUPERVISOR_PROGRAM_NUMPROCS="${SUPERVISOR_PROGRAM_NUMPROCS:-1}"
 
 # Render config templates.
-mkdir -p "$PROJECT_DIR/var/etc"
-dockerize -template "${SUPERVISOR_TMPL_CONF:-$IXC_DJANGO_DOCKER_DIR/etc/supervisord.tmpl.conf}:$PROJECT_DIR/var/etc/supervisord.conf"
-dockerize -template "${SUPERVISOR_INCLUDE_TMPL_CONF:-$IXC_DJANGO_DOCKER_DIR/etc/supervisord.include.tmpl.conf}:$PROJECT_DIR/var/etc/supervisord.include.conf"
+dockerize -template "${SUPERVISOR_TMPL_CONF:-$IXC_DJANGO_DOCKER_DIR/etc/supervisord.tmpl.conf}:$DIR/supervisord.conf"
+dockerize -template "${SUPERVISOR_INCLUDE_TMPL_CONF:-$IXC_DJANGO_DOCKER_DIR/etc/supervisord.include.tmpl.conf}:$DIR/supervisord.include.conf"
 
 if [[ -z "$@" ]]; then
-	exec supervisord --configuration "$PROJECT_DIR/var/etc/supervisord.conf"
+	exec supervisord --configuration "$DIR/supervisord.conf"
 else
-	exec supervisorctl --configuration "$PROJECT_DIR/var/etc/supervisord.conf" --serverurl "http://localhost:$SUPERVISOR_PORT" "$@"
+	exec supervisorctl --configuration "$DIR/supervisord.conf" --serverurl "http://localhost:$SUPERVISOR_PORT" "$@"
 fi
