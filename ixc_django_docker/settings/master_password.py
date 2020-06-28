2b99fb60828f0e734088bcbddafc6b781d4ad4cd
Tai Lee <real.human@mrmachine.net>
2020-06-28 15:53:58 +1000

    Fix master password settings.
    The master password auth backend was only checking `MASTER_PASSWORDS`, not `MASTER_PASSWORD` which was used in an earlier version.

--- ixc_django_docker/settings/master_password.py
+++ ixc_django_docker/settings/master_password.py
@@ -1,4 +1,6 @@
 AUTHENTICATION_BACKENDS += ('master_password.auth.ModelBackend', )
 INSTALLED_APPS += ('master_password', )
 MASTER_PASSWORD = os.environ.get('MASTER_PASSWORD')
-MASTER_PASSWORDS = {}
+
+if MASTER_PASSWORD:
+    MASTER_PASSWORDS = {MASTER_PASSWORD: None}
