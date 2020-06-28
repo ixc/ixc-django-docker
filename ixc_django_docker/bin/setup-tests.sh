2aa68feb787721964a70ba7df5ef6e1e22f6ffb2
Tai Lee <real.human@mrmachine.net>
2020-06-28 16:42:11 +1000

    Update `OVERRIDE_SETTINGS` in `setup-tests.sh`.
    Without this, we'd have `DOTENV=test` and `OVERRIDE_SETTINGS=develop.py` which caused (among possibly other things) double `test_test_` database prefix names.

--- ixc_django_docker/bin/setup-tests.sh
+++ ixc_django_docker/bin/setup-tests.sh
@@ -7,6 +7,7 @@ set -e
 
 export DOTENV=test
 export PGDATABASE="test_$PGDATABASE"
+export OVERRIDE_SETTINGS='test.py'
 export PS1="($PROJECT_NAME:$DOTENV) \u@\h:\w\n\\$ "
 export REUSE_DB=1
 export SETUP_TESTS=1
