eef0fd8d153d440a046bd1c96b097466c8e08a7d
Tai Lee <real.human@mrmachine.net>
2020-07-11 03:51:11 +1000

    Do not exit early from `npm-install.sh` or print errors.

--- ixc_django_docker/bin/npm-install.sh
+++ ixc_django_docker/bin/npm-install.sh
@@ -26,7 +26,7 @@ if [[ ! -s package.json.md5 ]] || ! md5sum --status -c package.json.md5 > /dev/n
 	echo "Node modules in '$DIR' directory are out of date."
 	if [[ -d node_modules ]]; then
 		echo 'Cleaning old Node modules directory.'
-		rm -rf node_modules/.* node_modules/*
+		rm -rf node_modules/.* node_modules/* 2>/dev/null || true
 	fi
 	if [[ -f yarn.lock ]]; then
 		yarn --non-interactive
