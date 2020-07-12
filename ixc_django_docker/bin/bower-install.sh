9ef8067c36a30c81db81824caa301c2f673b1c5e
Tai Lee <real.human@mrmachine.net>
2020-07-12 22:06:58 +1000

    Update `*-install.sh` scripts.
    - Do not delete existing directories on update. Image builds start with a clean slate. During development, bind mounts make this difficult and speed and incremental updates is preferred.
    - Use `npm install` instead of `npm ci` (which deletes `node_modules` directory). This allows incremental and lock file updates during development.

--- ixc_django_docker/bin/bower-install.sh
+++ ixc_django_docker/bin/bower-install.sh
@@ -23,11 +23,7 @@ fi
 touch bower.json.md5
 
 if [[ ! -s bower.json.md5 ]] || ! md5sum --status -c bower.json.md5 > /dev/null 2>&1; then
-	echo "Bower components in '$DIR' directory are out of date."
-	if [[ -d bower_components ]]; then
-		echo 'Removing old Bower components directory.'
-		rm -rf bower_components
-	fi
+	echo "Bower components in '$DIR' directory are out of date, 'bower.json' has been updated."
 	bower install --allow-root
 	md5sum bower.json > bower.json.md5
 fi
--- ixc_django_docker/bin/npm-install.sh
+++ ixc_django_docker/bin/npm-install.sh
@@ -23,15 +23,9 @@ fi
 touch package.json.md5
 
 if [[ ! -s package.json.md5 ]] || ! md5sum --status -c package.json.md5 > /dev/null 2>&1; then
-	echo "Node modules in '$DIR' directory are out of date."
-	if [[ -d node_modules ]]; then
-		echo 'Cleaning old Node modules directory.'
-		rm -rf node_modules/.* node_modules/* 2>/dev/null || true
-	fi
+	echo "Node modules in '$DIR' directory are out of date, 'package.json' has been updated."
 	if [[ -f yarn.lock ]]; then
 		yarn --non-interactive
-	elif [[ -f package-lock.json ]]; then
-		npm ci
 	else
 		npm install
 	fi
