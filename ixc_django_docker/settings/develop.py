91db04c9ba9bed8c75da788eb81459a5a1d0f88c
Tai Lee <real.human@mrmachine.net>
2020-07-24 16:30:14 +1000

    Re-enable autorefresh and staticfiles finders in develop settings.
    These are just not recommended for production.

--- ixc_django_docker/settings/develop.py
+++ ixc_django_docker/settings/develop.py
@@ -2,3 +2,8 @@ ALLOWED_HOSTS = ('*', )  # Don't validate the host header
 CSRF_COOKIE_SECURE = False  # Don't require HTTPS for CSRF cookie
 DEBUG = True  # Show detailed error pages when exceptions are raised
 SESSION_COOKIE_SECURE = False  # Don't require HTTPS for session cookie
+
+# WHITENOISE ##################################################################
+
+WHITENOISE_AUTOREFRESH = True
+WHITENOISE_USE_FINDERS = True
