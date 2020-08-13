5636a2b63f9a8ea25599082b289d9a690eef8f75
Tai Lee <real.human@mrmachine.net>
2020-08-13 16:38:20 +1000

    Do not configure the "user" alternative installation scheme when running with Docker.
    I doubt this was ever used and so should not be missed. For some unknown reason, it was causing problems with `entrypoint.sh` being unable to find `ixc_django_docker` during development.
    
    When running with Docker, it is now recommended to:
    
    - Install all Python packages in the image via system Python (outside the bind mounted project directory).
    - Build a development image on top of a production base image, in which you can install new and remove disused Python packages.
    - Rebuild the development image whenever `requirements.txt` changes.

--- ixc_django_docker/bin/entrypoint.sh
+++ ixc_django_docker/bin/entrypoint.sh
@@ -10,41 +10,6 @@ if [[ -d .git ]]; then
 fi
 
 if [[ -f /.dockerenv || -n "${DOCKER+1}" ]]; then
-	# When run via Docker, the only system site packages are the ones that we
-	# have installed, so we do not need a virtualenv for isolation. Using an
-	# isolated virtualenv would mean we have to reinstall everything during
-	# development, even when no versions have changed. Using a virtualenv
-	# created with `--system-site-packages` would mean we can avoid
-	# reinstalling everything, but pip would try to uninstall existing packages
-	# when we try to install a new version, which can fail with permissions
-	# errors (e.g. when running as an unprivileged user or when the image is
-	# read-only). The alternate installation user scheme avoids these problems
-	# by ignoring existing system site packages when installing a new version,
-	# instead of trying to uninstall them.
-	# See: https://pip.pypa.io/en/stable/user_guide/#user-installs
-
-	# Make `pip install --user` the default, to ensure we always install into
-	# the userbase directory.
-	mkdir -p "$HOME/.config/pip"
-	cat <<-EOF > "$HOME/.config/pip/pip.conf"
-	[install]
-	user = true
-	EOF
-
-	# Set location of userbase directory.
-	export PYTHONUSERBASE="$PROJECT_DIR/var/docker-pythonuserbase"
-
-	# Add userbase bin directory to PATH.
-	export PATH="$PYTHONUSERBASE/bin:$PATH"
-
-	# For some reason pip allows us to install sdist packages, but not editable
-	# packages, when these directories don't exist. So make sure they do.
-	# There's no easy way to detect the minor version of Python being used, so
-	# create directories for all supported versions. They don't hurt anything.
-	mkdir -p "$PYTHONUSERBASE/lib/python2.7/site-packages"
-	mkdir -p "$PYTHONUSERBASE/lib/python3.5/site-packages"
-	mkdir -p "$PYTHONUSERBASE/lib/python3.6/site-packages"
-
 	# On Docker for Mac, osxfs has performance issues when watching file system
 	# events. Detect Docker for Mac and export an environment variable that we
 	# can check to disable file system watches.
