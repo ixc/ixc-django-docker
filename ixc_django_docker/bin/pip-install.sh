561d798f54f851d2825076a02bec013cd25f6652
Tai Lee <real.human@mrmachine.net>
2020-08-13 20:13:51 +1000

    Do not disable cache for `pip-install.sh`.
    This breaks the buildkit cache volume during image build, but using `pip-install.sh` is a no-op when `requirements.txt` has not been updated when building a dev container. With `requirements.txt` being a pinned and flattened dependency tree, there's also little reason to disable the cache.
    
    We can of course also still disable pip cache with `PIP_NO_CACHE_DIR=1` interactively if we really want that behaviour back.

--- ixc_django_docker/bin/pip-install.sh
+++ ixc_django_docker/bin/pip-install.sh
@@ -12,7 +12,7 @@ cd "$DIR"
 if [[ -f requirements.txt ]]; then
 	if [[ ! -s requirements.txt.md5 ]] || ! md5sum --status -c requirements.txt.md5 > /dev/null 2>&1; then
 		echo "Python requirements in '$DIR' directory are out of date, 'requirements.txt' has been updated."
-		pip install --no-cache-dir -r requirements.txt
+		pip install -r requirements.txt
 		md5sum requirements.txt > requirements.txt.md5
 	fi
 fi
@@ -20,7 +20,7 @@ fi
 if [[ -f requirements-local.txt ]]; then
 	if [[ ! -s requirements-local.txt.md5 ]] || ! md5sum --status -c requirements-local.txt.md5 > /dev/null 2>&1; then
 		echo "Python requirements in '$DIR' directory are out of date, 'requirements-local.txt' has been updated."
-		pip install --no-cache-dir -r requirements-local.txt
+		pip install -r requirements-local.txt
 		md5sum requirements-local.txt > requirements-local.txt.md5
 	fi
 fi
