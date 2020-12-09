d2324a11e92a12680ada9b69cb928f84c9b8d64e
Tai Lee <real.human@mrmachine.net>
2020-12-09 21:53:57 +1100

    Update datadog env vars. `DATADOG_ENV` -> `DD_ENV`.

--- ixc_django_docker/bin/ddtrace.sh
+++ ixc_django_docker/bin/ddtrace.sh
@@ -2,11 +2,12 @@
 
 set -e
 
-export DATADOG_ENV="${DATADOG_ENV:-$PROJECT_NAME-${DATADOG_ENV:-$DOTENV}}"
+export DD_ENV="${DATADOG_ENV:-$PROJECT_NAME-${DATADOG_ENV:-$DOTENV}}"
 export DD_TRACE_ANALYTICS_ENABLED='true'
 
-# For ddtrace < 0.25.
-export DD_ANALYTICS_ENABLED="$DD_TRACE_ANALYTICS_ENABLED"
+# Legacy.
+export DATADOG_ENV="${DD_ENV}"
+export DD_ANALYTICS_ENABLED="${DD_TRACE_ANALYTICS_ENABLED}"
 
 echo 'Run command via Datadog Trace Client.'
 
