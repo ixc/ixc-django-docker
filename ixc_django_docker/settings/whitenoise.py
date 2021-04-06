7c261c227f71f19cc4b716676b7ca69bac3cbcd2
Tai Lee <real.human@mrmachine.net>
2021-04-06 15:18:40 +1000

    Use a unique name for the 'staticfiles.json' manifest, so we can run new and old versions of a codebase side by side with a shared volume.

--- ixc_django_docker/settings/whitenoise.py
+++ ixc_django_docker/settings/whitenoise.py
@@ -1,5 +1,13 @@
 INSTALLED_APPS += ('ixc_whitenoise', )
 
+# Use a unique name for the 'staticfiles.json' manifest, so we can run new and old
+# versions of a codebase side by side with a shared volume.
+IXC_WHITENOISE_MANIFEST_NAME = os.path.join(
+    os.environ.get('STATICFILES_MANIFEST') or os.path.join(
+        'staticfiles', '%s.json' % (os.environ.get('GIT_COMMIT') or '_unknown')
+    )
+)
+
 # Insert middleware above all except SecurityMiddleware, per the documentation
 try:
     index = MIDDLEWARE.index(
