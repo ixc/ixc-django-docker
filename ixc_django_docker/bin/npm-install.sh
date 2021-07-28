e7ba90020636f292e3e2135f2efd038cfaaf63bc
Tai Lee <real.human@mrmachine.net>
2021-07-28 22:36:48 +1000

    Again remove (contents of) `node_modules` directory.
    This should restore the original behaviour (always a fresh install) while still working when `node_modules` is a container volume.

--- ixc_django_docker/bin/npm-install.sh
+++ ixc_django_docker/bin/npm-install.sh
@@ -26,6 +26,9 @@ touch "package.json.md5.$UNAME"
 
 if [[ ! -s "package.json.md5.$UNAME" ]] || ! md5sum --status -c "package.json.md5.$UNAME" > /dev/null 2>&1; then
 	echo "Node modules in '$DIR' directory are out of date, 'package.json' has been updated."
+	if [[ -d node_modules ]]; then
+		rm -rf node_modules/* node_modules/.*
+	fi
 	if [[ -f yarn.lock ]]; then
 		yarn --frozen-lockfile --non-interactive
 	elif [[ -f package-lock.json ]]; then
