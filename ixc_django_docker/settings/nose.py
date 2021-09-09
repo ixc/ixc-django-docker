ec377975fb95b1b9ce3b0782eeaa7ef798e20249
Aram Dulyan <aram00@gmail.com>
2021-09-09 16:36:57 +1000

    Temporarily disable nose-progressive due to setuptools>=58 incompatibility
    See previous commit for details.
    
    Again, we could just pin setuptools to <58, but due to the upcoming
    deprecation of `use_2to3`, it's more future-proof to tackle the issue
    head-on.
    
    Since nose-progressive is mainly decorative, it seems appropriate to
    disable its usage for now, until we or someone else release a version of
    nose-progressive with Python 3 support that does not rely on 2to3.

--- ixc_django_docker/settings/nose.py
+++ ixc_django_docker/settings/nose.py
@@ -9,7 +9,8 @@ NOSE_ARGS = (
     # '--processes=-1',  # Automatically set to the number of cores
 )
 
-if sys.stdout.isatty():
-    NOSE_ARGS += ('--with-progressive', )  # See https://github.com/erikrose/nose-progressive
+# Temporarily disabled until nose-progressive adds support for setuptools>=58
+#if sys.stdout.isatty():
+#    NOSE_ARGS += ('--with-progressive', )  # See https://github.com/erikrose/nose-progressive
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'  # Default: django.test.runner.DiscoverRunner
