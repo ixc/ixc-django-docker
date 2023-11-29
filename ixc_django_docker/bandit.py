c9bf103e5e9fd7595962e316d171236844d02e75
Tai Lee <real.human@mrmachine.net>
2023-11-29 13:20:45 +1100

    Fix absolute import and improve Django<1.9 compatibility

--- ixc_django_docker/bandit.py
+++ ixc_django_docker/bandit.py
@@ -1,6 +1,9 @@
+from __future__ import absolute_import
+
 from bandit.backends.base import HijackBackendMixin
 from django.conf import settings
-from django.utils.module_loading import import_string
+
+from ixc_django_docker.django_compat import import_string
 
 OriginalEmailBackend = import_string(settings.HIJACKED_EMAIL_BACKEND)
 
